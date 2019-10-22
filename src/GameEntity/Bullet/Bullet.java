a8881c2d07d22b832b7a34bcf7aede8c1c523a8d
Truong Nguyen <truongnd792@gmail.com>
2019-10-22 15:44:21 +0700

    add doDomage to bullet

--- src/GameEntity/Bullet/Bullet.java
+++ src/GameEntity/Bullet/Bullet.java
@@ -1,6 +1,8 @@
 package GameEntity.Bullet;
 
+import GameEntity.Enemy.Enemy;
 import GameEntity.GameObject;
+import Program.Position;
 
 public abstract class Bullet extends GameObject {
     protected double speed;
@@ -18,6 +20,48 @@ public abstract class Bullet extends GameObject {
     	this.range = range;
     }
 
+    @Override
+    public void init() {
+        /*
+        TODO:
+        */
+    }
 
-    
+    @Override
+    public void doDestroy() {
+        /*
+        TODO:
+        */
+    }
+
+    @Override
+    public void setLocation(Position pos) {
+        /*
+        TODO:
+        */
+    }
+
+    @Override
+    public Position getLocation() {
+        return super.getLocation();
+    }
+
+    public void doDamage(Enemy enemy, Bullet bullet){
+        if(bullet.getLocation() == enemy.getLocation()) enemy.beAttacked();
+        this.doDestroy();
+    }
+
+    @Override
+    public String toString() {
+        return "Bullet{" +
+                "speed=" + speed +
+                ", damage=" + damage +
+                ", range=" + range +
+                ", position=" + position +
+                ", width=" + width +
+                ", height=" + height +
+                ", color='" + color + '\'' +
+                '}';
+    }
 }
+
