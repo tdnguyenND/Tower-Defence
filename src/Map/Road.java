78693b347bf457c00e1d1e97cb380d0833233c6f
tdnguyen <tdnguyen.uet@gmail.com>
2019-10-25 10:11:13 +0700

    map class

---
+++ src/Map/Road.java
@@ -0,0 +1,20 @@
+package Map;
+
+import Program.Position;
+
+public class Road extends Grid {
+    public Road(Position center) {
+        super(center);
+    }
+
+    public Road(int x, int y) {
+        super(x, y);
+    }
+
+    @Override
+    public String toString() {
+        return "Road{" +
+                "center=" + center +
+                '}';
+    }
+}
