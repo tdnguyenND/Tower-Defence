a677253415534685eed8d3d5548ca0eda032243b
Truong Nguyen <truongnd792@gmail.com>
2019-11-18 11:55:44 +0700

    tower images

--- src/GameEntity/GameTile/Tower/NormalTower.java
+++ src/GameEntity/GameTile/Tower/NormalTower.java
@@ -1,9 +1,16 @@
 package GameEntity.GameTile.Tower;
 
 import Map.Grid;
+import javafx.scene.image.Image;
+
+import java.io.File;
 
 public class NormalTower extends Tower implements TowerProperty{
     public NormalTower(Grid pos) {
         super(NORMAL_TOWER_HEIGHT, NORMAL_TOWER_WIDTH, pos, NORMAL_TOWER_RANGE, NORMAL_TOWER_ATTACK_RATE);
+
+        File file = new File("image/normal-tower-02.png");
+        String localURL = file.toURI().toString();
+        image = new Image(localURL);
     }
 }
